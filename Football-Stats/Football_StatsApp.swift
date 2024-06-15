71585eaa81f208ee93ac49b672e9a67e240ecaf9
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-15 22:23:20 +0800

    Initial Commit

---
+++ Football-Stats/Football_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_StatsApp.swift
+//  Football-Stats
+//
+//  Created by Kelvin on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Football_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
